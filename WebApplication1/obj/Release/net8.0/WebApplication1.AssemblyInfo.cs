d8d2031060c2ba45763301e6c84651f082058e43
Kirti Singh Chauhan <kirtisingh.chauhan@coditas.com>
2025-04-09 11:48:25 +0530

    second

--- WebApplication1/obj/Release/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Release/net8.0/WebApplication1.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5e581f74725f1fb2ca041fd8705f7a3c617ed3c")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
